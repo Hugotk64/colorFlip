d4951194f50cb5c141d2b5d25e9f6e2109d1afaa
Hugotk64 <hugoacv321@gmail.com>
2025-05-25 22:19:32 -0600

    style: button get hover effect and transition for smoothness.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,14 @@
 <style>
     :root {
         --color-body-bg: #F2F2F2;
+
         --color-menu-bg: #FFF;
         --color-menu-border: #222;
         --color-menu-shadow: #0000001a;
+
+        --color-button-bg: #98bc7a;
+        --color-button-txt: #FFF;
+        --color-button-hover: #334624;
     }
 
     :global(body){
@@ -35,5 +40,26 @@
         text-align: center;
         padding: 32px;
         width: 300px;
+    }
+
+    .button {
+        background-color: var(--color-button-bg);
+        
+        color: var(--color-button-txt);
+        text-decoration: none;
+        font-weight: bold;
+
+        margin: 16px 0;
+        padding: 16px;
+
+        display: flex;
+        flex-direction: column;
+        border-radius: 8px;
+
+        transition: background-color 2s ease;
+    }
+
+    .button:hover {
+        background-color: var(--color-button-hover);
     }
 </style>

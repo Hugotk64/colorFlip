b95586a813bcdaa5440fa2f18b84c46eb10eb071
Hugotk64 <hugoacv321@gmail.com>
2025-05-26 10:03:00 -0600

    add html for color convertion as a card shoing input, output and preview.

---
+++ src/routes/(colorConverter)/+layout.svelte
@@ -0,0 +1,24 @@
+<!-- Back button to home /-->
+<a href="/" class="backButton"> ← </a>
+
+<div class="card">
+    <!-- Preview picked color -->
+    <div class="preview"></div>
+    
+    <div class="content">
+        <!-- Group input elements label and input text -->
+        <div class="inputGroup">
+            <label for="input">input hex or rgb</label>
+            <input 
+            type="text"
+            id="input"
+            placeholder="example"
+            >
+        </div>
+        <!-- Group output elements label and output -->
+        <div class="outputGroup">
+            <label for="output">output hex or rgb</label>
+            <output class="valueOuput" id="output" for="input">output rgb or hex</output>
+        </div>
+    </div>
+</div>

3a00e0fcacebf2436055bfcefcb9b297c3222c76
Hugotk64 <hugoacv321@gmail.com>
2025-05-25 21:39:46 -0600

    style: body content center, full screen, set background and font.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,24 @@
 	<a href="/" class="button">HEX TO RGB</a>
 	<a href="/" class="button">RGB TO HEX</a>
 </div>
+
+<style>
+    :root {
+        --color-body-bg: #F2F2F2;
+    }
+
+    :global(body){
+        font-family: Helvetica,sans-serif;
+        background-color: var(--color-body-bg);
+        /* Center content */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* Cover full screen */
+        height: 100vh;
+        width: 100wh;
+        
+        margin: 0;
+    }
+
+</style>

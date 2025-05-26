7a14aa79142f6211671fa2227d266c184571cc78
Hugotk64 <hugoacv321@gmail.com>
2025-05-26 17:52:17 -0600

    Style: first modify html to remove elements to make it more alike a pantone card. set elements as column and size. and content gets flex grow to fill left space.

--- src/routes/(colorConverter)/+layout.svelte
+++ src/routes/(colorConverter)/+layout.svelte
@@ -5,40 +5,57 @@
 	<!-- Preview picked color -->
 	<div class="preview"></div>
 	<div class="content">
-		<!-- Group input elements label and input text -->
-		<div class="inputGroup">
-			<label for="input">input hex or rgb</label>
-			<input type="text" id="input" placeholder="example" />
-		</div>
-		<!-- Group output elements label and output -->
-		<div class="outputGroup">
-			<label for="output">output hex or rgb</label>
-			<output class="valueOuput" id="output" for="input">output rgb or hex</output>
-		</div>
+		<!-- Indicates the page -->
+		<label for="input">HEX TO RGB</label>
+        <!-- Input element -->
+		<input type="text" id="input" placeholder="#B2CD9C" />
+        <!-- Show the output -->
+		<output class="valueOuput" id="output" for="input">rgb(178, 205, 156)</output>
 	</div>
 </div>
 
 <style>
-    :global(body) {
-        font-family: Helvetica, sans-serif;
-        margin: 0;
-        /* Center content */
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        /* Full size */
-        height: 100vh;
-        width: 100vw;
-        /* set background picture, cover full screen and no repeat */
-        background-image: url(/src/lib/background.svg);
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
+	:global(body) {
+		font-family: Helvetica, sans-serif;
+		margin: 0;
+		/* Center content */
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		/* Full size */
+		height: 100vh;
+		width: 100vw;
+		/* set background picture, cover full screen and no repeat */
+		background-image: url(/src/lib/background.svg);
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
 
-    .card {
-        background: transparent;
-        border-radius: 8px;
-        box-shadow: 0 4px 16px #0009;
-        width: 336px;
-    }
+	.card {
+		background: transparent;
+		border-radius: 8px;
+		box-shadow: 0 4px 16px #0009;
+		/* Set elements as column */
+		display: flex;
+		flex-direction: column;
+
+		width: 250px;
+		height: 370px;
+	}
+    
+	.preview {
+		background-color: #333;
+        /* Cover full width and set height */
+		width: 100%;
+		height: 250px;
+	}
+
+	.content {
+		background-color: #fff;
+		padding: 12px;
+		
+		display: flex;
+        flex-grow: 1; /* If there is space left it fill it */
+		flex-direction: column;
+	}
 </style>

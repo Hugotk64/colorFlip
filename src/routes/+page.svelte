cd57102db35d087c4509469b5dc7cd7e143c7ca0
Hugotk64 <hugoacv321@gmail.com>
2025-05-26 09:52:26 -0600

    link home buttons to color converter pages.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div class="menu">
 	<h1>THE COLOR PROJECT</h1>
 
-	<a href="/" class="button">HEX TO RGB</a>
-	<a href="/" class="button">RGB TO HEX</a>
+	<a href="/hex-to-rgb" class="button">HEX TO RGB</a>
+	<a href="/rgb-to-hex" class="button">RGB TO HEX</a>
 </div>
 
 <style>

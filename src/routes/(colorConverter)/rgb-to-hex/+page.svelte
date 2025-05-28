8746f7396af7f6161ef32f28e3e51a979fd49cdc
Hugotk64 <hugoacv321@gmail.com>
2025-05-28 15:56:10 -0600

    feat: validate rgb numbers under 255, convert each color to hex, and decimal alpha to hex.

--- src/routes/(colorConverter)/rgb-to-hex/+page.svelte
+++ src/routes/(colorConverter)/rgb-to-hex/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
     import { input,output,previewColor } from "$lib/stores";
 
+    function toHex(value: number): string {
+        // take a number turn base 16 if single digit at cero at start
+        return value.toString(16).padStart(2,'0').toUpperCase();
+    }
+    function alphaToHex(alpha: number): string {
+        // Multiply alpha by 255 round the number and turn to base 16, add 0 if sigle digit
+        return Math.round(alpha * 255).toString(16).padStart(2, '0').toUpperCase();
+    }
     $: {
         // Remove spaces
         const rgb = $input.trim();
@@ -24,7 +32,14 @@
             // if a is no defined get number and convert to float else null
             const a = match[4] !== undefined ? parseFloat(match[4]) : null;
 
-            
+            // Validate r g b are valid numbers between 0 and 255
+            if ( [r,g,b].some(v => v < 0 || v > 255) ) {
+                output.set("RGB values must be between 0 and 255.");
+            } else {
+                const hex = `#${toHex(r)}${toHex(g)}${toHex(b)}${a !== null ? alphaToHex(a) : ''}`;
+                output.set(hex);
+                previewColor.set(rgb);
+            }
         }
     }
 </script>

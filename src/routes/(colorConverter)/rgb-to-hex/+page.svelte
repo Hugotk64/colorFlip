9d1dfca021bc35bdbc93c81a62085ec683690f0c
Hugotk64 <hugoacv321@gmail.com>
2025-05-28 13:02:29 -0600

    feat: use match to validate rgbRegex and to extract each color.

--- src/routes/(colorConverter)/rgb-to-hex/+page.svelte
+++ src/routes/(colorConverter)/rgb-to-hex/+page.svelte
@@ -10,12 +10,21 @@
         // Optinal alpha.
         // Case insensitivity R-r
         const rgbRegex = /^rgba?\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})(?:\s*,\s*(0|1|0?\.\d+))?\s*\)$/i;
-
+        const match = rgb.match(rgbRegex);
         if (!rgb) {
             output.set('');
             previewColor.set('rgb(178, 205, 156)');
-        } else if (!rgbRegex.test(rgb)) {
+        } else if (!match) {
             output.set('Invalid RGB or RGBA format.');
+        } else {
+            // Extract each color from rgba
+            const r = Number(match[1]);
+            const g = Number(match[2]);
+            const b = Number(match[3]);
+            // if a is no defined get number and convert to float else null
+            const a = match[4] !== undefined ? parseFloat(match[4]) : null;
+
+            
         }
     }
 </script>
